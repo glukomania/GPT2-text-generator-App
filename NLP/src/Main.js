61e66effb509a9ec1217f7a5e2a65d9a1460a899
Natalia Shuvaeva <glukomani@gmail.com>
2023-02-18 21:48:24 +0100

    First commit

---
+++ NLP/src/Main.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
