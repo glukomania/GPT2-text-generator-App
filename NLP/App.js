2b2e0d91e26584b676a8da6fed18fb3c3f3289af
Natalia Shuvaeva <glukomani@gmail.com>
2023-02-18 23:50:26 +0100

    gpt2 chat kinda works

--- NLP/App.js
+++ NLP/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { Main } from './src/Main'
+import { View } from 'react-native'
+import Main from './src/Main'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
+      {console.log('==== starting ====')}
+      <StatusBar style="auto" />
       <Main />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
--- NLP/src/Main.js
+++ NLP/src/Main.js
@@ -1,10 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { StyleSheet, Text, ScrollView, TextInput, View, Pressable } from 'react-native'
 
 export const Main = () => {
+  const [response, setResponsse] = useState('')
+  const [question, setQuestion] = useState('')
+
+  async function query(data) {
+    const API_TOKEN = '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'
+
+    const authData = {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+      method: 'POST',
+      body: {
+        data: JSON.stringify(data),
+        num_return_sequences: 30,
+        set_seed: 100,
+        max_length: 300,
+      },
+    }
+
+    const modelURL = 'https://api-inference.huggingface.co/models/gpt2'
+    const res = await fetch(modelURL, authData, 500)
+    const result = await res.json()
+
+    setResponsse(result[0].generated_text)
+    // return result
+  }
+
+  const onGoPress = () => {
+    question.length > 0 && query(question)
+  }
+
+  // useEffect(() => {
+  //   try {
+  //     query('Who are you? ')
+  //   } catch (e) {
+  //     console.log('e')
+  //   }
+  // }, [])
+
   return (
-    <View style={{ flexDirection: 'row' }}>
-      <Text>Hello world</Text>
+    <View
+      style={{
+        padding: '20%',
+        flexDirection: 'colomn',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <TextInput
+        onChangeText={setQuestion}
+        value={question}
+        placeholder="type your question here"
+        keyboardType="text"
+        style={{
+          margin: '10%',
+          marginBottom: '30%',
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          padding: '5%',
+        }}
+      />
+      <Pressable
+        onPress={onGoPress}
+        style={{
+          borderWidth: 1,
+          backgroundColor: 'yellow',
+          padding: '5%',
+          borderRadius: '15%',
+          marginBottom: '30%',
+        }}
+      >
+        <Text>Go</Text>
+      </Pressable>
+      {response.length > 0 && <Text>{response}</Text>}
     </View>
   )
 }
+
+export default Main
